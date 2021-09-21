ef3959ac5a956e11bcfe6c91d101cd858bc3e4b8
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-09-21 07:44:35 +0700

    Initial commit

---
+++ lib/Games/TabNoun.pm
@@ -0,0 +1,15 @@
+# no code
+## no critic: TestingAndDebugging::RequireUseStrict
+package Games::TabNoun;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Select nouns from list of words, as fast as possible
+
+=head1 SYNOPSIS
+
+See L<tabnoun>.
